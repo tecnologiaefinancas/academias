537dad6d5419fb613c7c71dcd4be23138590bb22
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-04-08 18:53:31 -0300

    Adding env file

--- src/main/java/com/tecnologiaefinancas/academias/config/CorsConfig.java
+++ src/main/java/com/tecnologiaefinancas/academias/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.tecnologiaefinancas.academias.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Value("${frontend.urltf}")
+    private String frontendUrlTF;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "https://www.tecnologiaefinancas.com/academias/")
+                .allowedOrigins(frontendUrl, frontendUrlTF)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
